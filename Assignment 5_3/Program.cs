a581a3c5101405c8a72371d6c0dce5c960f1240c
Sunil Dagur <sunildagur566@gmail.com>
2023-12-05 18:51:42 +0530

    Add project files.

---
+++ Assignment 5_3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter a number: ");
+        string userInput = Console.ReadLine();
+
+        try
+        {
+            int number = int.Parse(userInput);
+            Console.WriteLine("Successfully converted to integer: " + number);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("An error occurred: " + ex.Message);
+        }
+    }
+}
+
